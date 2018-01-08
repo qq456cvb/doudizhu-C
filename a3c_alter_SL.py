fix batch normalization bugs

a3c_alter_SL.py
@@ -362,18 +362,20 @@ if __name__ == '__main__':
     TRAIN = args.train
     sess = tf.Session(graph=graph_sl)
     saver = tf.train.Saver(max_to_keep=50)
-    # saver.restore(sess, "./Model/accuracy_bugfixed_lr0001_1110/model-900.ckpt")
+    saver.restore(sess, "./Model/accuracy_fake_minor/model-9800")
 
-    file_writer = tf.summary.FileWriter('accuracy_fake_minor', sess.graph)
+    # file_writer = tf.summary.FileWriter('accuracy_fake_minor', sess.graph)
 
-    filelist = [ f for f in os.listdir('./accuracy_fake_minor') ]
-    for f in filelist:
-        os.remove(os.path.join('./accuracy_fake_minor', f))
+    # filelist = [ f for f in os.listdir('./accuracy_fake_minor') ]
+    # for f in filelist:
+    #     os.remove(os.path.join('./accuracy_fake_minor', f))
 
     logger = Logger()
     # TODO: support batch training
+    # test_cards = [i for i in range(3, 18)]
+    # test_cards = np.array(test_cards *)
     if TRAIN:
-        sess.run(tf.global_variables_initializer())
+        # sess.run(tf.global_variables_initializer())
         for i in range(epoches_train):
             e.reset()
             e.prepare()

inference.py
@@ -71,17 +71,19 @@ if __name__ == '__main__':
     saver = tf.train.Saver()
 
     e = env.Env()
+    test_cards = [i for i in range(3, 16)]
+    test_cards = np.array(test_cards * 4 + [16, 17])
     with tf.Session() as sess:
-        saver.restore(sess, './Model/SL-0105/model-9950.ckpt')
-        for i in range(100):
+        saver.restore(sess, './Model/accuracy_fake_minor/model-9800')
+        for i in range(1):
             e.reset()
-            e.prepare()
-
+            e.prepare_manual(test_cards)
+        
             r = 0
             done = False
             while not done:
                 idx = e.get_role_ID()
-                if idx == 2:
+                if idx == 4:
                     print("current handcards: ", to_char(e.get_curr_handcards()))
                     intention = read_cards_input()
                 else:
@@ -89,13 +91,13 @@ if __name__ == '__main__':
                     curr_cards_value = e.get_curr_handcards()
                     curr_cards_char = to_char(curr_cards_value)
                     last_cards_value = e.get_last_outcards()
-                    # print("curr_cards = ", curr_cards_value)
+                    print("curr_cards = ", curr_cards_char)
                     # print("last_cards = ", last_cards_value)
                     last_category_idx = e.get_last_outcategory_idx()
-                    # last_cards_char = to_char(last_cards_value)
+                    last_cards_char = to_char(last_cards_value)
                     # mask = get_mask(curr_cards_char, action_space, last_cards_char)
 
-                    input_single, input_pair, input_triple, input_quadric = get_masks(curr_cards_char, to_char(last_cards_value))
+                    input_single, input_pair, input_triple, input_quadric = get_masks(curr_cards_char, last_cards_char if last_cards_value.size > 0 else None)
 
                     s = e.get_state()
                     s = np.reshape(s, [1, -1])
@@ -107,7 +109,7 @@ if __name__ == '__main__':
 
                         decision_active_output = sess.run(network.fc_decision_active_output,
                             feed_dict={
-                                network.training: False,
+                                network.training: True,
                                 network.input_state: s,
                                 network.input_single: np.reshape(input_single, [1, -1]),
                                 network.input_pair: np.reshape(input_pair, [1, -1]),
@@ -125,7 +127,7 @@ if __name__ == '__main__':
                         # give actual response
                         response_active_output = sess.run(network.fc_response_active_output,
                             feed_dict={
-                                network.training: False,
+                                network.training: True,
                                 network.input_state: s,
                                 network.input_single: np.reshape(input_single, [1, -1]),
                                 network.input_pair: np.reshape(input_pair, [1, -1]),
@@ -137,6 +139,7 @@ if __name__ == '__main__':
                         response_active_output[response_mask[decision_active] == 0] = 0
                         response_active = np.argmax(response_active_output)
 
+                        seq_length = 0
                         # next sequence length
                         if active_category_idx == Category.SINGLE_LINE.value or \
                                 active_category_idx == Category.DOUBLE_LINE.value or \
@@ -145,7 +148,7 @@ if __name__ == '__main__':
                                 active_category_idx == Category.THREE_TWO_LINE.value:
                             seq_length_output = sess.run(network.fc_sequence_length_output,
                                 feed_dict={
-                                    network.training: False,
+                                    network.training: True,
                                     network.input_state: s,
                                     network.input_single: np.reshape(input_single, [1, -1]),
                                     network.input_pair: np.reshape(input_pair, [1, -1]),
@@ -178,7 +181,7 @@ if __name__ == '__main__':
                             = sess.run([network.fc_decision_passive_output,
                                         network.fc_response_passive_output, network.fc_bomb_passive_output],
                                         feed_dict={
-                                            network.training: False,
+                                            network.training: True,
                                             network.input_state: s,
                                             network.input_single: np.reshape(input_single, [1, -1]),
                                             network.input_pair: np.reshape(input_pair, [1, -1]),
@@ -186,6 +189,7 @@ if __name__ == '__main__':
                                             network.input_quadric: np.reshape(input_quadric, [1, -1])
                                         })
                         
+                        # print(decision_passive_output)
                         decision_passive_output = decision_passive_output[0]
                         decision_passive_output[decision_mask == 0] = 0
                         decision_passive = np.argmax(decision_passive_output)
@@ -222,9 +226,11 @@ if __name__ == '__main__':
                                 intention = np.concatenate([intention, to_value(inference_minor_cards(last_category_idx, s, curr_cards_char.copy(), 
                                     sess, network, get_seq_length(last_category_idx, last_cards_value)))])
                     
-                print("intention is ", end='')
-                print(to_char(intention))
-                r, done, category_idx = e.step_manual(intention)
+                # print("intention is ", end='')
+                # print(to_char(intention))
+                # r, done, category_idx = e.step_manual(intention)
+                intention, r, category_idx = e.step_auto()
+                print("auto intention is ", intention)
 
 
 

network_SL.py
@@ -347,11 +347,14 @@ class CardNetwork:
             with tf.name_scope("total_loss"):
                 self.loss = tf.to_float(self.is_active) * self.active_loss + (1 - tf.to_float(self.is_active)) * self.passive_loss
 
-            with tf.name_scope("optimize"):
-                self.optimize = trainer.minimize(self.loss)
+            # update moving avg/var in batch normalization!
+            extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+            with tf.control_dependencies(extra_update_ops):
+                with tf.name_scope("optimize"):
+                    self.optimize = trainer.minimize(self.loss)
 
-            with tf.name_scope("optimize_fake_response"):
-                self.optimize_fake = trainer.minimize(tf.reduce_sum(self.active_response_loss))
+                with tf.name_scope("optimize_fake_response"):
+                    self.optimize_fake = trainer.minimize(tf.reduce_sum(self.active_response_loss))
 
             local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
             self.gradients = tf.gradients(self.loss, local_vars)