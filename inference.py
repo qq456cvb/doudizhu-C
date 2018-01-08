aed93595b4ffe31b6da2b33f9239ae8f664f867e
Neil You <neilyou@qq.com>
2018-01-08 12:48:16 +0800

    add inference part

---
+++ inference.py
@@ -0,0 +1,231 @@
+from network_SL import CardNetwork
+import tensorflow as tf
+import numpy as np
+import sys
+from card import Category
+from utils import get_mask_alter, get_masks, to_char, to_value, get_seq_length, give_cards_without_minor
+sys.path.insert(0, './build/Release')
+import env
+
+
+def read_cards_input():
+    intention = sys.stdin.readline()
+    intention = intention.split()
+    return np.array(to_value(intention))
+
+
+# return char minor cards output
+def inference_minor_util(s, handcards, sess, network, num, is_pair):
+    outputs = []
+    input_single, input_pair, input_triple, input_quadric = get_masks(handcards, None)
+    for i in range(num):
+        response_active_output = sess.run(network.fc_response_active_output,
+                feed_dict = {
+                    network.training: False,
+                    network.input_state: s,
+                    network.input_single: np.reshape(input_single, [1, -1]),
+                    network.input_pair: np.reshape(input_pair, [1, -1]),
+                    network.input_triple: np.reshape(input_triple, [1, -1]),
+                    network.input_quadric: np.reshape(input_quadric, [1, -1])
+            })
+        
+        # give minor cards
+        response_active_output = response_active_output[0]
+        if is_pair:
+            response_active_output[input_pair == 0] = 0
+        else:
+            response_active_output[input_single == 0] = 0
+        response_active = np.argmax(response_active_output)
+
+        # convert network output to char cards
+        handcards.remove(to_char(response_active + 3))
+        if is_pair:
+            handcards.remove(to_char(response_active + 3))
+
+        # update mask for the next loop
+        input_single, input_pair, input_triple, input_quadric = get_masks(handcards, None)
+
+        # save to output
+        outputs.append(to_char(response_active + 3))
+        if is_pair:
+            outputs.append(to_char(response_active + 3))
+    return outputs
+
+
+def inference_minor_cards(category, s, handcards, sess, network, seq_length):
+    if category == Category.THREE_ONE.value:
+        return inference_minor_util(s, handcards, sess, network, 1, False)
+    if category == Category.THREE_TWO.value:
+        return inference_minor_util(s, handcards, sess, network, 1, True)
+    if category == Category.THREE_ONE_LINE.value:
+        return inference_minor_util(s, handcards, sess, network, seq_length, False)
+    if category == Category.THREE_TWO_LINE.value:
+        return inference_minor_util(s, handcards, sess, network, seq_length, True)
+    if category == Category.FOUR_TWO.value:
+        return inference_minor_util(s, handcards, sess, network, 2, False)
+
+
+if __name__ == '__main__':
+    g = tf.get_default_graph()
+    network = CardNetwork(54 * 6, tf.train.AdamOptimizer(learning_rate=0.0001), "SLNetwork")
+    saver = tf.train.Saver()
+
+    e = env.Env()
+    with tf.Session() as sess:
+        saver.restore(sess, './Model/SL-0105/model-9950.ckpt')
+        for i in range(100):
+            e.reset()
+            e.prepare()
+
+            r = 0
+            done = False
+            while not done:
+                idx = e.get_role_ID()
+                if idx == 2:
+                    print("current handcards: ", to_char(e.get_curr_handcards()))
+                    intention = read_cards_input()
+                else:
+                
+                    curr_cards_value = e.get_curr_handcards()
+                    curr_cards_char = to_char(curr_cards_value)
+                    last_cards_value = e.get_last_outcards()
+                    # print("curr_cards = ", curr_cards_value)
+                    # print("last_cards = ", last_cards_value)
+                    last_category_idx = e.get_last_outcategory_idx()
+                    # last_cards_char = to_char(last_cards_value)
+                    # mask = get_mask(curr_cards_char, action_space, last_cards_char)
+
+                    input_single, input_pair, input_triple, input_quadric = get_masks(curr_cards_char, to_char(last_cards_value))
+
+                    s = e.get_state()
+                    s = np.reshape(s, [1, -1])
+
+                    is_active = (last_cards_value.size == 0)
+                    if is_active:
+                        # first get mask
+                        decision_mask, response_mask, _, length_mask = get_mask_alter(curr_cards_char, [], False, last_category_idx)
+
+                        decision_active_output = sess.run(network.fc_decision_active_output,
+                            feed_dict={
+                                network.training: False,
+                                network.input_state: s,
+                                network.input_single: np.reshape(input_single, [1, -1]),
+                                network.input_pair: np.reshape(input_pair, [1, -1]),
+                                network.input_triple: np.reshape(input_triple, [1, -1]),
+                                network.input_quadric: np.reshape(input_quadric, [1, -1])
+                            })
+
+                        # make decision depending on output
+                        decision_active_output = decision_active_output[0]
+                        decision_active_output[decision_mask == 0] = 0
+                        decision_active = np.argmax(decision_active_output)
+                        
+                        active_category_idx = decision_active + 1
+
+                        # give actual response
+                        response_active_output = sess.run(network.fc_response_active_output,
+                            feed_dict={
+                                network.training: False,
+                                network.input_state: s,
+                                network.input_single: np.reshape(input_single, [1, -1]),
+                                network.input_pair: np.reshape(input_pair, [1, -1]),
+                                network.input_triple: np.reshape(input_triple, [1, -1]),
+                                network.input_quadric: np.reshape(input_quadric, [1, -1])
+                            })
+
+                        response_active_output = response_active_output[0]
+                        response_active_output[response_mask[decision_active] == 0] = 0
+                        response_active = np.argmax(response_active_output)
+
+                        # next sequence length
+                        if active_category_idx == Category.SINGLE_LINE.value or \
+                                active_category_idx == Category.DOUBLE_LINE.value or \
+                                active_category_idx == Category.TRIPLE_LINE.value or \
+                                active_category_idx == Category.THREE_ONE_LINE.value or \
+                                active_category_idx == Category.THREE_TWO_LINE.value:
+                            seq_length_output = sess.run(network.fc_sequence_length_output,
+                                feed_dict={
+                                    network.training: False,
+                                    network.input_state: s,
+                                    network.input_single: np.reshape(input_single, [1, -1]),
+                                    network.input_pair: np.reshape(input_pair, [1, -1]),
+                                    network.input_triple: np.reshape(input_triple, [1, -1]),
+                                    network.input_quadric: np.reshape(input_quadric, [1, -1])
+                                })
+
+                            seq_length_output = seq_length_output[0]
+                            seq_length_output[length_mask[decision_active][response_active] == 0] = 0
+                            seq_length = np.argmax(seq_length_output) + 1
+                        
+                        # give main cards
+                        intention = give_cards_without_minor(response_active, last_cards_value, active_category_idx, seq_length)
+
+                        # then give minor cards
+                        if active_category_idx == Category.THREE_ONE.value or \
+                                active_category_idx == Category.THREE_TWO.value or \
+                                active_category_idx == Category.THREE_ONE_LINE.value or \
+                                active_category_idx == Category.THREE_TWO_LINE.value or \
+                                active_category_idx == Category.FOUR_TWO.value:
+                            intention = np.concatenate([intention, to_value(inference_minor_cards(active_category_idx, s, curr_cards_char.copy(), sess, network, seq_length))])
+                    else:
+                        is_bomb = False
+                        if len(last_cards_value) == 4 and len(set(last_cards_value)) == 1:
+                            is_bomb = True
+                        decision_mask, response_mask, bomb_mask, _ = get_mask_alter(curr_cards_char, to_char(last_cards_value),
+                                                                                 is_bomb, last_category_idx)
+
+                        decision_passive_output, response_passive_output, bomb_passive_output \
+                            = sess.run([network.fc_decision_passive_output,
+                                        network.fc_response_passive_output, network.fc_bomb_passive_output],
+                                        feed_dict={
+                                            network.training: False,
+                                            network.input_state: s,
+                                            network.input_single: np.reshape(input_single, [1, -1]),
+                                            network.input_pair: np.reshape(input_pair, [1, -1]),
+                                            network.input_triple: np.reshape(input_triple, [1, -1]),
+                                            network.input_quadric: np.reshape(input_quadric, [1, -1])
+                                        })
+                        
+                        decision_passive_output = decision_passive_output[0]
+                        decision_passive_output[decision_mask == 0] = 0
+                        decision_passive = np.argmax(decision_passive_output)
+
+                        if decision_passive == 0:
+                            intention = np.array([])
+                        elif decision_passive == 1:
+                            is_passive_bomb = True
+
+                            bomb_passive_output = bomb_passive_output[0]
+                            bomb_passive_output[bomb_mask == 0] = 0
+                            bomb_passive = np.argmax(bomb_passive_output)
+
+                            # converting 0-based index to 3-based value
+                            intention = np.array([bomb_passive + 3] * 4)
+
+                        elif decision_passive == 2:
+                            is_passive_king = True
+                            intention = np.array([16, 17])
+                        elif decision_passive == 3:
+                            response_passive_output = response_passive_output[0]
+                            response_passive_output[response_mask == 0] = 0
+                            response_passive = np.argmax(response_passive_output)
+
+                            # there is an offset when converting from 0-based index to 1-based index
+                            bigger = response_passive + 1
+
+                            intention = give_cards_without_minor(bigger, last_cards_value, last_category_idx, None)
+                            if active_category_idx == Category.THREE_ONE.value or \
+                                    active_category_idx == Category.THREE_TWO.value or \
+                                    active_category_idx == Category.THREE_ONE_LINE.value or \
+                                    active_category_idx == Category.THREE_TWO_LINE.value or \
+                                    active_category_idx == Category.FOUR_TWO.value:
+                                intention = np.concatenate([intention, to_value(inference_minor_cards(last_category_idx, s, curr_cards_char.copy(), 
+                                    sess, network, get_seq_length(last_category_idx, last_cards_value)))])
+                    
+                print("intention is ", end='')
+                print(to_char(intention))
+                r, done, category_idx = e.step_manual(intention)
+
+
+
+
