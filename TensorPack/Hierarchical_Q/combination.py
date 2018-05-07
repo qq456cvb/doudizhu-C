65b8b355f08559cbf29eb36d92d28216a0fc05ce
NeilYou <447626601@qq.com>
2018-05-07 23:41:32 +0800

    fix bugs; add subsampling and heuristic decompose

--- TensorPack/Hierarchical_Q/combination.py
+++ TensorPack/Hierarchical_Q/combination.py
@@ -6,7 +6,7 @@ if os.name == 'nt':
     sys.path.insert(0, '../../build/Release')
 else:
     sys.path.insert(0, '../../build.linux')
-from env import get_combinations
+from env import get_combinations_recursive, get_combinations_nosplit
 from utils import get_mask_onehot60, get_mask
 from card import action_space, Card, action_space_category, Category
 
@@ -16,39 +16,39 @@ def dancing_link():
     env.reset()
     env.prepare()
     # print(env.get_handcards())
-    # cards = env.get_handcards()
-    cards = ['3', '3', '3']
+    cards = env.get_handcards()[:10]
+    # cards = ['3', '3', '3']
     mask = get_mask_onehot60(cards, action_space, None).astype(np.uint8)
 
-    # augment mask
-    # TODO: known issue: 555444666 will not decompose into 5554 and 66644
-    augmented_action_space = action_space.copy()
-    singles = []
-    single_mask = get_mask(cards, action_space_category[Category.SINGLE])
-    for i in range(single_mask.size - 2):
-        if single_mask[i] > 0:
-            augmented_action_space += [action_space_category[Category.SINGLE][i]] * 3
-            for j in range(1, 4):
-                tmp = np.zeros([60])
-                tmp[i * 4 + j] = 1
-                singles.append(tmp)
-
-    doubles = []
-    double_mask = get_mask(cards, action_space_category[Category.DOUBLE])
-    for i in range(double_mask.size):
-        if double_mask[i] > 0:
-            augmented_action_space += [action_space_category[Category.DOUBLE][i]]
-            tmp = np.zeros([60])
-            tmp[i * 4 + 2:i * 4 + 4] = 1
-            doubles.append(tmp)
-
-    mask = np.concatenate([mask, np.stack(singles + doubles)])
+    # # augment mask
+    # # TODO: known issue: 555444666 will not decompose into 5554 and 66644
+    # augmented_action_space = action_space.copy()
+    # singles = []
+    # single_mask = get_mask(cards, action_space_category[Category.SINGLE])
+    # for i in range(single_mask.size - 2):
+    #     if single_mask[i] > 0:
+    #         augmented_action_space += [action_space_category[Category.SINGLE][i]] * 3
+    #         for j in range(1, 4):
+    #             tmp = np.zeros([60])
+    #             tmp[i * 4 + j] = 1
+    #             singles.append(tmp)
+    #
+    # doubles = []
+    # double_mask = get_mask(cards, action_space_category[Category.DOUBLE])
+    # for i in range(double_mask.size):
+    #     if double_mask[i] > 0:
+    #         augmented_action_space += [action_space_category[Category.DOUBLE][i]]
+    #         tmp = np.zeros([60])
+    #         tmp[i * 4 + 2:i * 4 + 4] = 1
+    #         doubles.append(tmp)
+    #
+    # mask = np.concatenate([mask, np.stack(singles + doubles)])
     # print(Card.char2onehot60(cards).astype(np.uint8))
-    combs = get_combinations(mask, Card.char2onehot60(cards).astype(np.uint8))
+    combs = get_combinations_nosplit(mask, Card.char2onehot60(cards).astype(np.uint8))
     print(len(combs))
     for comb in combs:
         for idx in comb:
-            print(augmented_action_space[idx], end=', ')
+            print(action_space[idx], end=', ')
         print()
 
 
@@ -57,12 +57,12 @@ def recursive():
     env.reset()
     env.prepare()
     # print(env.get_handcards())
-    cards = env.get_handcards()[:20]
+    cards = env.get_handcards()[:10]
     # cards = ['3', '3',  '4', '4']
     mask = get_mask_onehot60(cards, action_space, None).reshape(len(action_space), 15, 4).sum(-1).astype(np.uint8)
     valid = mask.sum(-1) > 0
     cards_target = Card.char2onehot60(cards).reshape(-1, 4).sum(-1).astype(np.uint8)
-    combs = get_combinations(mask, cards_target, valid)
+    combs = get_combinations_recursive(mask, cards_target, valid)
     print(len(combs))
     for comb in combs:
         for idx in comb:
@@ -72,4 +72,5 @@ def recursive():
 
 if __name__ == '__main__':
     # print(action_space[16:60])
+    dancing_link()
     recursive()
